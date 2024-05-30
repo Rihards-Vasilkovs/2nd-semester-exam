a3bc89afc0c9b2611a9fdc65948ff710893d2d9e
Rihards <eaa23riva@students.eaaa.dk>
2024-05-30 12:13:38 +0200

    adding content through json file

--- app.js
+++ app.js
@@ -3,6 +3,25 @@
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
 // Function to initialize the Web App
-function initApp() {
+async function initApp() {
   console.log("initApp: app.js is running 🎉"); // Log to the console that the app is running
+  const posts = await getProjects(); //Executes code by calling function
+  displayProjects(posts); // Calls the displayProjects function
+}
+
+async function getProjects() {
+  const response = await fetch ("https://wp.rihardsmedia.dk/wp-json/wp/v2/posts?acf_format=standard");
+  const data = await response.json();
+  return data;
+}
+
+function displayProjects(posts) {
+  const projectsList = document.querySelector("#projects-list");
+  
+  for (const post of posts) {
+    projectsList.insertAdjacentHTML(
+      "beforeend",
+      `<li>${post.title.rendered}</li>`
+    )
+  }
 }
